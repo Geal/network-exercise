5e9f5077eb79205c5f3ae36b5b70b70ef5a97434
Geoffroy Couprie <geo.couprie@gmail.com>
2016-11-14 18:16:16 +0100

    multi client

--- server.cpp
+++ server.cpp
@@ -9,11 +9,11 @@
 
 using namespace std;
 
-void connection_thread(int& sock);
+void connection_thread(int id, int sock);
 
 int main() {
-    int socket_desc , client_sock , c;
-    struct sockaddr_in server , client;
+    int socket_desc, c;
+    struct sockaddr_in server;
 
     socket_desc = socket(AF_INET , SOCK_STREAM , 0);
     if (socket_desc == -1) {
@@ -36,32 +36,37 @@ int main() {
     cout << "Waiting for incoming connections..." << endl;
     c = sizeof(struct sockaddr_in);
 
-    client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
-    if (client_sock < 0) {
-        perror("accept failed");
-        return 1;
+    int id = 0;
+    while(true) {
+      struct sockaddr_in client;
+      int client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
+      if (client_sock < 0) {
+          perror("accept failed");
+          return 1;
+      }
+      cout << "[" << id << "] Connection accepted" << endl;
+      std::thread t(connection_thread, id, client_sock);
+      t.detach();
+      id++;
     }
-    cout << "Connection accepted" << endl;
-    std::thread t(connection_thread, std::ref(client_sock));
-
-    t.join();
     return 0;
 }
 
-void connection_thread(int& sock) {
+void connection_thread(int id, int sock) {
     int read_size;
     char client_message[2000];
 
     while( (read_size = recv(sock , client_message , 2000 , 0)) > 0 ) {
         client_message[read_size] = 0;
-        cout << "client sent:" << client_message << endl;
+        cout << "[" << id << "]:" << client_message << endl;
         write(sock , client_message , read_size);
     }
 
     if(read_size == 0) {
-        cout << "Client disconnected" << endl;
+        cout << "[" << id << "] disconnected" << endl;
         fflush(stdout);
     } else if(read_size == -1) {
-        perror("recv failed");
+        cout << "[" << id << "]recv failed" << endl;
+        fflush(stdout);
     }
 }
