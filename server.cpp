492f3efa12859428d22d33d6d7a9ece41e873738
Geoffroy Couprie <geo.couprie@gmail.com>
2016-11-14 18:36:08 +0100

    set non blocking sockets on the server

--- server.cpp
+++ server.cpp
@@ -3,8 +3,10 @@
 #include<sys/socket.h>
 #include<arpa/inet.h>
 #include<unistd.h>
+#include <fcntl.h>
 #include <iostream>
 #include <thread>
+#include <chrono>
 #include "queue.h"
 
 using namespace std;
@@ -21,6 +23,8 @@ int main() {
     }
     cout << "Socket created" << endl;
 
+    fcntl(socket_desc, F_SETFL, O_NONBLOCK);
+
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
     server.sin_port = htons( 8888 );
@@ -30,6 +34,7 @@ int main() {
         return 1;
     }
     cout << "bind done" << endl;
+
 
     listen(socket_desc , 3);
 
@@ -41,6 +46,11 @@ int main() {
       struct sockaddr_in client;
       int client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
       if (client_sock < 0) {
+          //cout << "client sock: " << client_sock << " errno: " << errno << endl;
+          if (errno == EWOULDBLOCK || errno == EAGAIN) {
+            this_thread::sleep_for(chrono::milliseconds(50));
+            continue;
+          }
           perror("accept failed");
           return 1;
       }
@@ -56,17 +66,24 @@ void connection_thread(int id, int sock) {
     int read_size;
     char client_message[2000];
 
-    while( (read_size = recv(sock , client_message , 2000 , 0)) > 0 ) {
+    while(true) {
+        read_size = recv(sock , client_message , 2000 , 0);
+        if(read_size == 0) {
+            cout << "[" << id << "] disconnected" << endl;
+            fflush(stdout);
+            break;
+        } else if(read_size == -1) {
+          if (errno == EWOULDBLOCK || errno == EAGAIN) {
+            this_thread::sleep_for(chrono::milliseconds(50));
+            continue;
+          }
+          cout << "[" << id << "] recv failed: " << errno << endl;
+          fflush(stdout);
+          break;
+        }
+
         client_message[read_size] = 0;
         cout << "[" << id << "]:" << client_message << endl;
         write(sock , client_message , read_size);
-    }
-
-    if(read_size == 0) {
-        cout << "[" << id << "] disconnected" << endl;
-        fflush(stdout);
-    } else if(read_size == -1) {
-        cout << "[" << id << "]recv failed" << endl;
-        fflush(stdout);
     }
 }
